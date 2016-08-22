bdf5a241bd813ecc05a18170222756fe7e8c4657
Reshad Hosseini <reshadhosseini@Reshads-MacBook-Pro.local>
2016-08-22 12:36:59 +0430

    For initializing in split or merge use estimate

--- mixest/distributions/mixture/mixture_mergeinit.m
+++ mixest/distributions/mixture/mixture_mergeinit.m
@@ -60,12 +60,20 @@ function [theta, store] = mixture_mergeinit(D, idx1, idx2, theta, options, data,
         % function, we use the default merging method
         method = get_method(options_mergeinit, Didx1Name, '');
         if strcmp(method, 'default')
-
-            w1 = theta.p(idx1);
-            w2 = theta.p(idx2);
-            theta.D{idx1} = Didx1.sumparam(...
-                Didx1.scaleparam(w1/(w1+w2),theta.D{idx1}), ...
-                Didx1.scaleparam(w2/(w1+w2),theta.D{idx2}));            
+            if true
+                options.verbosity = 0;
+                data = mxe_readdata(data);
+                h = D.weighting(theta, data);
+                data.weight = sum(h([idx1 idx2],:),1);
+                DidxM = D.component(idx1);
+                theta.D{idx1} = DidxM.estimate(data, options);
+            else
+                w1 = theta.p(idx1);
+                w2 = theta.p(idx2);
+                theta.D{idx1} = Didx1.sumparam(...
+                    Didx1.scaleparam(w1/(w1+w2),theta.D{idx1}), ...
+                    Didx1.scaleparam(w2/(w1+w2),theta.D{idx2}));
+            end
         else
             error('mixture.mergeinit: Method ''%s'' not recognized for component(%d) parameters', method, idx)
         end
